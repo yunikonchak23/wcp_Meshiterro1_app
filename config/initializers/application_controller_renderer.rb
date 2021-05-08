02eef8b75c26eda08d5e0210cfcd2c5650ccf2f4
yunikonchak23 <uetamago.btssuga@gmail.com>
2021-05-08 22:33:14 +0000

    [Add]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2e022ea49be0c450ed93dead072a1b4381219f081616354c85e6002f1999d39686332a7b6ae63f689e52420fc45006865e6ef60b7745782dd7ebe2c2a30bfc7c'
